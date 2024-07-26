c8bb1e5ceb3174437cb8e3f94ecb64f875aa96ad
philramirez <pramirez@tollbrothers.com>
2024-07-26 14:36:12 -0400

    chore: add button stylesheet

--- src/mui/primitives/Button/Button.js
+++ src/mui/primitives/Button/Button.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import MuiButton from '@mui/material/Button'
+import styles from './Button.module.scss'
 
 const Button = ({ children, ...rest }) => {
   return (
-    <MuiButton disableElevation {...rest}>
+    <MuiButton className={styles.button} disableElevation {...rest}>
       {children}
     </MuiButton>
   )
