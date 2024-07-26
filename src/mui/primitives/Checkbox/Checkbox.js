3728f167c454f379417c0bffa2e5bb7515c99836
philramirez <pramirez@tollbrothers.com>
2024-07-26 14:03:49 -0400

    chore: commit checkbox

--- src/mui/primitives/Checkbox/Checkbox.js
+++ src/mui/primitives/Checkbox/Checkbox.js
@@ -3,6 +3,7 @@ import MuiCheckbox from '@mui/material/Checkbox'
 import styles from './Checkbox.module.scss'
 
 const Checkbox = ({ children, ...rest }) => {
+  console.log('styles.checkbox', styles.checkbox)
   return (
     <MuiCheckbox className={styles.checkbox} {...rest}>
       {children}
