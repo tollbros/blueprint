070229f2a38c196a882824c3a53b36d18e3c931c
philramirez <pramirez@tollbrothers.com>
2024-07-26 14:06:31 -0400

    chore: fix index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import Checkbox from './mui/primitives/Checkbox'
-import Button from './mui/primitives/Button'
+import Checkbox from './mui/primitives/Checkbox/Checkbox'
+import Button from './mui/primitives/Button/Button'
 import ThemeProvider from './themes/ThemeProvider'
 
 export { Checkbox, Button, ThemeProvider }
