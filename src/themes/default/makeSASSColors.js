6cfec98989854a0f2b01d32edfaf11fbedc0cfaa
philramirez <pramirez@tollbrothers.com>
2025-01-15 13:46:55 -0500

    fix: add colors script

---
+++ src/themes/default/makeSASSColors.js
@@ -0,0 +1,36 @@
+import { promises as fs } from 'fs'
+import { defaultTheme } from './theme'
+import process from 'process'
+
+const flattenToSassVars = (obj, parentKey = '') => {
+  const result = {};
+
+  for (const [key, value] of Object.entries(obj)) {
+    const newKey = parentKey ? `${parentKey}-${key}` : key;
+
+    if (typeof value === 'object' && value !== null) {
+      Object.assign(result, flattenToSassVars(value, newKey));
+    } else {
+      result[`$tb-${newKey}`] = value;
+    }
+  }
+  return result
+}
+
+const makeSASSColors = async () => {
+  const palette = defaultTheme.palette
+  // At this point result is pretty much this
+  // {
+  //    '$tb-primary': '#1976d2',
+  //    '$tb-secondary': '#dc004e'
+  // }
+  const sassVarsObj = flattenToSassVars({ palette });
+  // Need to transform it to SCSS format
+  const sassVars = Object.entries(sassVarsObj).map(([key, value]) => `${key}: ${value};`).join('\n')
+
+  const currentWorkingDirectory = process.cwd()
+
+  await fs.writeFile(`${currentWorkingDirectory}/styles/modules/_colors.scss`, sassVars);
+}
+
+makeSASSColors()
