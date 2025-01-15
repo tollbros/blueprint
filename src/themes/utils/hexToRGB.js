354ca1771b2a56dd22aa9276e51badca90e9f1d2
philramirez <pramirez@tollbrothers.com>
2025-01-15 14:46:51 -0500

    fix: add RGB for hex values

---
+++ src/themes/utils/hexToRGB.js
@@ -0,0 +1,9 @@
+const hexToRgba = (hex) => {
+  hex = hex.replace('#', '');
+  const r = parseInt(hex.substring(0, 2), 16);
+  const g = parseInt(hex.substring(2, 4), 16);
+  const b = parseInt(hex.substring(4, 6), 16);
+  return `${r}, ${g}, ${b}`;
+};
+
+export default hexToRgba;
